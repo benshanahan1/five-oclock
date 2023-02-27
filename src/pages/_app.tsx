325226e3394c736a534b78841ffaf222c3cb48ac
Benjamin Shanahan <benshanahan1@gmail.com>
2023-02-27 12:23:41 -0800

    Add cronitor site monitor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useCronitor } from '@cronitorio/cronitor-rum-nextjs'
+
+const debug = process.env.NODE_ENV !== "production";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Load the Cronitor tracker once in your app
+  useCronitor("7b656641cca7796bef032620b6464196", { debug });
   return <Component {...pageProps} />
 }
