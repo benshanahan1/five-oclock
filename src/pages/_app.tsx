eecb41d230e097cd3ad042247feec0f0c61e1ceb
Benjamin Shanahan <benshanahan1@gmail.com>
2024-03-25 11:57:32 -0700

    Remove cronitor monitoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useCronitor } from '@cronitorio/cronitor-rum-nextjs'
-
-const debug = process.env.NODE_ENV !== "production";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  // Load the Cronitor tracker once in your app
-  useCronitor("7b656641cca7796bef032620b6464196", { debug });
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
